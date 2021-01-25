87ca6580212581c77417e22d05308193c899d55b
evtn <k01419q45@ya.ru>
2021-01-25 19:15:09 +0700

    Added setup files

---
+++ matlog/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of matlog package.
+"""
+
+__title__ = 'matlog'
+__description__ = 'matlog — Boolean Algebra Parser+Solver'
+__version__ = '2.0.1'
+__author__ = 'evtn'
+__author_email__ = 'g@evtn.ru'
+__license__ = 'GNUv3'
+__url__ = 'https://github.com/evtn/matlog'
