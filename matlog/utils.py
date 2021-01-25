4d36710771baab73bc0751941ac668be0482ff5a
evtn <k01419q45@ya.ru>
2021-01-25 16:49:40 +0700

    utils.Table class

--- matlog/utils.py
+++ matlog/utils.py
@@ -10,6 +10,28 @@ def combinations(letters: List[Any]) -> None:
     count = len(letters)
     for i in range(2 ** count):
         yield {
-            letters[x]: cut((2 ** x) & i)
+            letters[x]: cut_literal((2 ** x) & i)
             for x in range(count)
         }
+
+
+class Table:
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return "\n".join([
+            " ".join(self.data["identifiers"]),
+            *map(
+                lambda row: " ".join(
+                    map(
+                        lambda i: str(row[i]),
+                        self.data["identifiers"]
+                    )
+                ),
+                self.data["values"]
+            )
+        ])
+
+    def __repr__(self):
+        return str(self)
