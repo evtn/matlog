4b4a7e88cbb3888f872558d3887d6158fbbd4a18
evtn <k01419q45@ya.ru>
2021-01-26 17:40:11 +0700

    Updated version

--- matlog/__version__.py
+++ matlog/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of matlog package.
 
 __title__ = 'matlog'
 __description__ = 'matlog — Boolean Algebra Parser+Solver'
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 __author__ = 'evtn'
 __author_email__ = 'g@evtn.ru'
 __license__ = 'GNUv3'
